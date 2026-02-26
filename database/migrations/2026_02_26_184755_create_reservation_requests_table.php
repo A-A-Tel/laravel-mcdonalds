370a8abfdd7a6d58d5b7bce88efbffd15634d7ea
A-A-Tel <anouaranthonytel@gmail.com>
2026-02-26 20:54:59 +0100

    added laravel migrations

---
+++ database/migrations/2026_02_26_184755_create_reservation_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->integer('people_count');
+            $table->string('message', 128);
+            $table->boolean('allowed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation_requests');
+    }
+};
