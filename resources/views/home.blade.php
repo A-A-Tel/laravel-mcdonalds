2f99db8184aa32624437a18f57a4605411df76bd
A-A-Tel <anouaranthonytel@gmail.com>
2026-02-26 22:41:48 +0100

    changed project structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="/css/style.css">
         <title>{{ config('app.name', 'Laravel') }}</title>
     </head>
     <body class="">
-        Cheese
+
     </body>
 </html>
