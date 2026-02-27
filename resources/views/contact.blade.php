926575c44cfab28ed4cb8a0795124a893ce34de7
A-A-Tel <anouaranthonytel@gmail.com>
2026-02-27 18:10:22 +0100

    added contact info

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,12 +6,20 @@
     <link rel="stylesheet" href="/css/style.css">
     <title>{{ config('app.name', 'Laravel') }} - Contact</title>
 </head>
-<body>
+<body class="contact-body">
 @include('components.header', ['page' => 'contact'])
 
-<main>
-
-</main>
+<div class="generic-text-box">
+    <h2>Neem contact met ons op via deze methodes</h2>
+    <span>
+        <img src="/img/phone.svg" alt="phone">
+        <a target="_blank" href="tel:+310205642666">+31 020-5642666</a>
+    </span>
+    <span>
+        <img src="/img/email.svg" alt="email">
+        <a target="_blank" href="mailto:gastenrelaties@nl.mcd.com">gastenrelaties@nl.mcd.com</a>
+    </span>
+</div>
 
 @include('components.footer')
 </body>
