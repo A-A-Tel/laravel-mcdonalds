075e9f178555f7adb810f882ce2bfdbcab52f8f6
A-A-Tel <anouaranthonytel@gmail.com>
2026-02-27 10:40:49 +0100

    added locations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,9 +12,18 @@
         <main>
             <div class="sensation-container">
                 <img src="/img/chicken-sensation.png" alt="chicken">
-                <div>
+                <div class="generic-text-box">
                     <h2>Bij de McDonald's hebben wij jouw nieuwe favoriete burger.</h2>
                     <p>Hier hebben we een gerecht voor iedereen hun smaken. Zin in kip? Beef? Salade? Bij de McDonald's hebben we het. Check ook onze app voor allerlei kortingen en deals. </p>
+                </div>
+            </div>
+
+            <div class="mcdonalds-world-container">
+                <img src="/img/mcdonalds-world.png" alt="McDonalds-buildings">
+                <div class="generic-text-box">
+                    <h2>Wij hebben restaurants over de hele wereld.</h2>
+                    <p>Al bent u aan het genieten in de staten, de prachtige landschappen van Japan, de lage landen langs the Europese kust of the kleine eilanden van Hawaii, er is altijd een McDonald's bij u in de buurt.</p>
+                    <a target="_blank" href="https://www.google.com/maps/search/mcdonalds/">Bekijk locaties</a>
                 </div>
             </div>
         </main>
