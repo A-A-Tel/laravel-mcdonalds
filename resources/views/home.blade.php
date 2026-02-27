eb44e2d438b922b2b49ff66200aaf4561bf831a7
A-A-Tel <anouaranthonytel@gmail.com>
2026-02-27 13:49:17 +0100

    added mccafe section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/css/style.css">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }} - Home</title>
     </head>
     <body>
         @include('header', ['page' => 'home'])
@@ -23,7 +23,14 @@
                 <div class="generic-text-box">
                     <h2>Wij hebben restaurants over de hele wereld.</h2>
                     <p>Al bent u aan het genieten in de staten, de prachtige landschappen van Japan, de lage landen langs the Europese kust of the kleine eilanden van Hawaii, er is altijd een McDonald's bij u in de buurt.</p>
-                    <a target="_blank" href="https://www.google.com/maps/search/mcdonalds/">Bekijk locaties</a>
+                    <a target="_blank   " href="https://www.google.com/maps/search/mcdonalds/">Bekijk locaties</a>
+                </div>
+            </div>
+
+            <div class="cafe-container">
+                <div class="generic-text-box">
+                    <h2>McCafé bij u in de buurt.</h2>
+                    <p>McDonald's bied veel andere restaurants uit met McCafé. Hier zal een van onze geweldige en vriendelijke crewleden uw koffie persoonlijk maken. Wij bieden veel keuzes in soorten koffie, soorten melk en versnaperingen zoals appeltaarten en brownies</p>
                 </div>
             </div>
         </main>
