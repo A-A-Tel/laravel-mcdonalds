b0bbf6a66900d37740e2ebe5b4fefc26879c9e19
A-A-Tel <anouaranthonytel@gmail.com>
2026-03-23 12:56:11 +0100

    fixed reservation bug

--- resources/views/components/reservation_request.blade.php
+++ resources/views/components/reservation_request.blade.php
@@ -11,6 +11,6 @@
     <div class="status">
         <h2>Status:</h2>
         <!--suppress CssUnresolvedCustomProperty -->
-        <div style="background: var(--{{$reservation_request->processed ? 'primary-light' : 'secondary-light'}})">{{$reservation_request->processed ? 'Toegestaan' : 'Niet toegestaan'}}</div>
+        <div style="background: var(--{{$reservation_request->allowed ? 'primary-light' : 'secondary-light'}})">{{$reservation_request->allowed ? 'Toegestaan' : 'Niet toegestaan'}}</div>
     </div>
 </div>
